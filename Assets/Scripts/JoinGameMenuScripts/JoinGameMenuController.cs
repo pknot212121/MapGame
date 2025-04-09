43dcd99df49e4124d3fc52ee83c68a58f36b7957
VelkoLakh <mc333gamer@gmail.com>
2025-04-09 11:32:26 +0200

    bbb

--- Assets/Scripts/JoinGameMenuScripts/JoinGameMenuController.cs
+++ Assets/Scripts/JoinGameMenuScripts/JoinGameMenuController.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fusion;
 
 public class JoinGameMenuController : MonoBehaviour
 {
+    public void StartGameClick()
+    {
+
+    }
+
     public void BackClick()
     {
         SceneManager.LoadScene("Menu", LoadSceneMode.Single);
