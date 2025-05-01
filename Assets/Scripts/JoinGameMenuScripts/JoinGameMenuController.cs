e651c78674a210a5880d6bed01a70fcb12242eb3
pknot212121 <pknot@student.agh.edu.pl>
2025-05-01 21:12:21 +0200

    Change minimum length of session name

--- Assets/Scripts/JoinGameMenuScripts/JoinGameMenuController.cs
+++ Assets/Scripts/JoinGameMenuScripts/JoinGameMenuController.cs
@@ -27,7 +27,7 @@ public class JoinGameMenuController : MonoBehaviour
     [UnityEngine.Scripting.Preserve] public void StartGameClick()
     {
         string name = "";
-        if(gameNameInput.text.Length < 6)
+        if(gameNameInput.text.Length < 2)
         {
             var random = new System.Random();
             char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
